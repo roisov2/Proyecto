a078944c33892a89673edcfc65f09ecb014a45bf
roisov2 <roisov8@gmail.com>
2023-05-11 23:28:47 -0300

    hasta aca pude llegar

--- Programa/BackEnd/src/main/java/com/portfolio/roiso/controlador/PersonaControler.java
+++ Programa/BackEnd/src/main/java/com/portfolio/roiso/controlador/PersonaControler.java
@@ -4,6 +4,7 @@ import com.portfolio.roiso.Entidad.Persona;
 import com.portfolio.roiso.interfas.IPersonaServicio;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,15 +15,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin (origins = "http://localhost:4200")
 public class PersonaControler {
     @Autowired IPersonaServicio ipersonaService;
     
-    @GetMapping("personas/traer")
+    @GetMapping("/personas/traer")
     public List<Persona> getPersona(){
         return ipersonaService.getPersona();
     }
    
-    @PostMapping("personas/crear")
+    @PostMapping("/personas/crear")
     public String createPersona(@RequestBody Persona persona){
         ipersonaService.savePersona(persona);
         return "La persona fue creada correctamente";
--- Programa/FrontEnd/src/app/app.module.ts
+++ Programa/FrontEnd/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
+import {HttpClient, HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -30,6 +32,7 @@ import { FooterComponent } from './components/footer/footer.component';
     BrowserModule,
     AppRoutingModule,
     NgCircleProgressModule.forRoot({}),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
